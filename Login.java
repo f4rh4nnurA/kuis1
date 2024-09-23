7c7303b76ab21a4bd00d6b186997de8807e9fc14
f4rh4nnurA <144897656+f4rh4nnurA@users.noreply.github.com>
2024-09-23 10:19:07 +0700

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,18 @@
+class Login {
+    Registrasi dataRegistrasi;
+
+    public Login(Registrasi dataRegistrasi) {
+        this.dataRegistrasi = dataRegistrasi;
+    }
+
+   
+    public boolean autentikasi(String inputUsername, String inputPassword) {
+        if (inputUsername.equals(dataRegistrasi.getUsername()) && inputPassword.equals(dataRegistrasi.getPassword())) {
+            System.out.println("Login berhasil! Selamat datang, " + dataRegistrasi.getPeserta().getNamaPeserta());
+            return true;
+        } else {
+            System.out.println("Login gagal! Username atau password salah.");
+            return false;
+        }
+    }
+}
---
+++ Ujian.java
@@ -0,0 +1,29 @@
+class Ujian {
+    int jumlahSoal;
+    int nilai;
+    String status;
+    String tanggalUjian;
+
+
+    public Ujian(int jumlahSoal, int nilai, String tanggalUjian) {
+        this.jumlahSoal = jumlahSoal;
+        this.nilai = nilai;
+        this.tanggalUjian = tanggalUjian;
+    }
+
+    public boolean statuskanKelulusan() {
+        if (nilai >= 80) {
+            status = "Lulus";
+            return true;
+        } else {
+            status = "Tidak Lulus";
+            return false;
+        }
+    }
+
+    public void tampilkanInfoUjian() {
+        System.out.println("Jumlah Soal         : " + jumlahSoal);
+        System.out.println("Nila                : " + nilai);
+        System.out.println("Status Kelulusan    : " + status);
+    }
+}
